afb63d2d2d8f90a2e73967c107e3896dfe48b502
JadeMin <alt0000@klartnet.ml>
2023-06-17 23:55:54 +0900

    clear

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import initBot from "./bot.ts";
-import initWeb from "./web.ts";
-
-initBot();
-initWeb();
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const sleep = (ms: number): Promise<NodeJS.Timeout> => new Promise(resovle => setTimeout(resovle, ms));
-
-export const getRandom = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
