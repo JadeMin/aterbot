ec75b54a8169ed114144e2743fb2b7800218a461
JadeMin <alt0000@klartnet.ml>
2023-07-05 04:04:48 +0900

    chore: add web res head `allow-methods` and `PING`

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,7 @@ const PORT = process.PORT || 5500;
 const server = HTTP.createServer((request, response) => {
 	response.writeHead(200, {
 		"Access-Control-Allow-Origin": "no-bitches.lol",
-		"Access-Control-Allow-Methods": "GET, OPTIONS",
+		"Access-Control-Allow-Methods": "GET, PING, OPTIONS",
 		"Content-Type": "text/html"
 	});
 	response.end("<h3>Copy me, the url above!</h3>");
