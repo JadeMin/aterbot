e31b9963dd15ee2f4618ffed921595f2d0198618
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:09:57 +0900

    chore: migration package manager `npm` to `pnpm`

---
+++ .out/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo -e "\\e[0;31m" "WARNING: Please set" "\\e[40m" "Workspace Template" "\\e[0m\\e[0;31m" "to" "\\e[40m" "Blank Repl" "\\e[0m\\e[0;31m" "before importing." "\\e[0m"
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-18_x
+		pkgs.nodePackages.pnpm
 	];
 }
