693be9bf267ac696568bfacf79bf1941e59eab27
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:22:51 +0900

    chore: added TypeScript placeholder

--- .out/build.sh
+++ .out/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-echo -e "\\e[0;31m" "WARNING: Please set" "\\e[40m" "Workspace Template" "\\e[0m\\e[0;31m" "to" "\\e[40m" "Blank Repl" "\\e[0m\\e[0;31m" "before importing." "\\e[0m"
+echo
+echo -e "\\e[0;31m" "WARNING: Please set" "\\e[40m" "Language" "\\e[0m\\e[0;31m" "to" "\\e[40m" "Blank Repl" "\\e[0m\\e[0;31m" "before importing."
+echo -e "\\e[0m" "Skipping compile process..."
+echo
