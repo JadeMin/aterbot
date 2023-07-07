b3a9250030c3ecd4e0ed3f4def7624d0ca552373
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:45:24 +0900

    chore: test

--- .config/build.sh
+++ .config/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-RESET=$"\e[0m"
-RED=$"\e[0;31m"
-YELLOW=$"\e[0;33m"
-BG_BLACK=$"\e[40m"
+DIR="$(dirname "$0")"
 
-echo 
+. ./coloring.sh
+
+
+
+echo
 echo "${YELLOW}WARNING: Please set ${BG_BLACK}Language${RESET}${YELLOW} to ${BG_BLACK}Blank Repl${RESET}${YELLOW} before importing."
 echo "Skipping compile process...${RESET}"
 echo
---
+++ .config/coloring.sh
@@ -0,0 +1,4 @@
+export RESET="\e[0m"
+export RED="\e[0;31m"
+export YELLOW="\e[1;33m"
+export BG_BLACK="\e[40m"
