1451dbedeed70c8fc257b09d3a6923149885c987
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:36:23 +0900

    chore: change CLI color

--- .config/build.sh
+++ .config/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-export RESET = "\e[0m"
-export RED = "\e[0;31m"
-export YELLOW = "\e[0;33m"
-export BG_BLACK = "\e[40m"
+RESET = $"\e[0m"
+RED = $"\e[0;31m"
+YELLOW = $"\e[0;33m"
+BG_BLACK = $"\e[40m"
 
 echo 
 echo "${YELLOW}WARNING: Please set ${BG_BLACK}Language${RESET}${YELLOW} to ${BG_BLACK}Blank Repl${RESET}${YELLOW} before importing."
