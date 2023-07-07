58eb8d1226fec25b509ed9b6303038dfe7253be5
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:48:17 +0900

    chore: add `clear` to hide command inputs

--- .config/build.sh
+++ .config/build.sh
@@ -5,6 +5,7 @@ DIR="$(dirname "$0")"
 
 
 
+clear
 echo
 echo "${YELLOW}WARNING: Please set ${BG_BLACK} Language ${RESET}${YELLOW} to ${BG_BLACK} Blank Repl ${RESET}${YELLOW} before importing."
 echo "Skipping compile process...${RESET}"
