a4934535a571abf2affc4bbea6c5c0350f55da58
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:51:35 +0900

    chore: move empty echo for readability

--- .config/build.sh
+++ .config/build.sh
@@ -7,5 +7,5 @@ DIR="$(dirname "$0")"
 
 clear
 echo "${YELLOW}WARNING: Please set ${BG_BLACK} Language ${RESET}${YELLOW} to ${BG_BLACK} Blank Repl ${RESET}${YELLOW} before importing."
-echo
 echo "${RESET}Skipping compile process..."
+echo
