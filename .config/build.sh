7f1f039a05c1983ebb96d53ac5372f8242e3fc11
JadeMin <alt0000@klartnet.ml>
2023-07-08 00:37:24 +0900

    chore: fix variable definitions

--- .config/build.sh
+++ .config/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-RESET = $"\e[0m"
-RED = $"\e[0;31m"
-YELLOW = $"\e[0;33m"
-BG_BLACK = $"\e[40m"
+RESET=$"\e[0m"
+RED=$"\e[0;31m"
+YELLOW=$"\e[0;33m"
+BG_BLACK=$"\e[40m"
 
 echo 
 echo "${YELLOW}WARNING: Please set ${BG_BLACK}Language${RESET}${YELLOW} to ${BG_BLACK}Blank Repl${RESET}${YELLOW} before importing."
