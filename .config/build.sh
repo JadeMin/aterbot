7aded074effe580754a1208d6e5646bc035f4a37
JadeMin <alt0000@klartnet.ml>
2023-07-08 12:31:05 +0900

    chore: echo

--- .config/build.sh
+++ .config/build.sh
@@ -6,6 +6,7 @@ DIR="$(dirname "$0")"
 
 
 clear
-echo "${YELLOW}WARNING: Please set ${BG_BLACK} Language ${RESET}${YELLOW} to ${BG_BLACK} Blank Repl ${RESET}${YELLOW} before importing."
-echo "WARNING: Skipping compile process...${RESET}"
-echo
+echo $YELLOW
+echo "WARNING: Please set ${BG_BLACK} Language ${RESET}${YELLOW} to ${BG_BLACK} Blank Repl ${RESET}${YELLOW} before importing."
+echo "WARNING: Skipping compile process..."
+echo $RESET
