c2d1defdff8978e7ea16d3e382b1e9444e194ce6
JadeMin <36400787+JadeMin@users.noreply.github.com>
2023-08-11 07:23:40 +0900

    add code.ts

---
+++ src/app/code.ts
@@ -0,0 +1,34 @@
+const validateRE = {
+	host: /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/,
+	port: /^[0-9]{1,5}$/,
+	username: /^[a-zA-Z0-9_]{3,16}$/
+};
+
+
+
+export const generateCode = (host: string, port: string, username: string): string => {
+	const code = {
+		client: {
+			host: "Please enter your server ip or url",
+			port: "Please enter your server port",
+			username: "Please enter the bot's username you want"
+		},
+		
+		"! DO NOT EDIT BELOW !": "ONLY IF YOU KNOW WHAT YOU ARE DOING",
+		logLevel: ["error", "log", "debug"],
+		action: {
+			commands: ["forward", "back", "left", "right", "jump"],
+			holdDuration: 5000,
+			retryDelay: 15000
+		}
+	};
+	code.client.host = host;
+	code.client.port = port;
+	code.client.username = username;
+
+	return JSON.stringify(code, null, '\t');
+};
+
+export const validateInputs = (host: string, port: string, username: string): boolean => {
+	return validateRE.host.test(host) && validateRE.port.test(port) && validateRE.username.test(username);
+};
